4a39eb4e668fd2d383b8a3c0650729649b81550c
Sandro Gerber <sandro-gerber@outlook.com>
2021-02-19 09:45:29 +0100

    Add login form

---
+++ components/atoms/TextField.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+import { VTextField } from 'vuetify/lib'
+
+import { Component, Prop } from 'vue-class-decorator'
+
+@Component
+export default class TextField extends VTextField {
+  @Prop({ default: true }) private filled!: boolean | string
+  @Prop({ default: 'secondary' }) private color!: string
+}
+</script>
--- pages/login.vue
+++ pages/login.vue
@@ -1,7 +1,63 @@
 <template>
-  <v-row>
-    <v-col class="text-center">
-      <h1>Login Page</h1>
-    </v-col>
-  </v-row>
+  <v-container pa-6>
+    <v-row no-gutters>
+      <v-col class="text-center">
+        <text-field
+          name="username"
+          label="Username"
+          id="username"
+          color="secondary"
+          prepend-icon="mdi-account"
+          @click:append="dostuf"
+        ></text-field>
+      </v-col>
+    </v-row>
+    <v-row no-gutters>
+      <v-col class="text-center">
+        <password-field name="password" label="Password" id="password">
+        </password-field>
+      </v-col>
+    </v-row>
+    <v-row no-gutters>
+      <v-col class="text-center">
+        <v-btn width="180px" color="primary">Login</v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-row no-gutters>
+          <v-divider></v-divider>
+        </v-row>
+        <v-row no-gutters>
+          <v-spacer></v-spacer>
+          <span class="caption"
+            >Don't have an account yet?
+            <nuxt-link to="/registration">Sign up here</nuxt-link>.
+          </span>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-class-decorator'
+
+import TextField from '../components/atoms/TextField.vue'
+import PasswordField from '../components/molecules/PasswordField.vue'
+
+@Component({
+  components: {
+    TextField,
+    PasswordField,
+  },
+})
+export default class Login extends Vue {
+  private showPassword: boolean = false
+
+  dostuf() {
+    console.log('done')
+  }
+}
+</script>
