573f5e3622d0ab24e73f49bd905e52e34a3f058f
Sandro Gerber <sandro-gerber@outlook.com>
2021-02-19 01:25:04 +0100

    Add page transition

--- layouts/default.vue
+++ layouts/default.vue
@@ -56,3 +56,17 @@ export default class Default extends Vue {
   ]
 }
 </script>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition-property: opacity;
+  transition-timing-function: ease-in-out;
+  transition-duration: 500ms;
+}
+
+.page-enter,
+.page-leave-to {
+  opacity: 0;
+}
+</style>
