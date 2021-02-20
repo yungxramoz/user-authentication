6235a9dd972a6375f64d1c6f4590e70e55b4b57f
Sandro Gerber <sandro-gerber@outlook.com>
2021-02-20 17:45:36 +0100

    Update logo and images

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,23 @@
 <template>
   <v-row>
-    <v-col class="text-center">
-      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
+    <v-col class="text-center mt-5">
+      <div class="d-flex flex-column justify-space-between align-center mb-5">
+        <v-img
+          contain
+          lazy-src="/sm.png"
+          max-height="196"
+          max-width="302"
+          src="/sm.png"
+        >
+          <template v-slot:placeholder>
+            <v-row class="fill-height ma-0" align="center" justify="center">
+              <v-progress-circular
+                indeterminate
+                color="grey lighten-5"
+              ></v-progress-circular>
+            </v-row> </template
+        ></v-img>
+      </div>
       <blockquote class="blockquote">
         &#8220;First, solve the problem. Then, write the code.&#8221;
         <footer>
