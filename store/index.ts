683c8db81d386b9fdcf5a861e452b4bc2c05ea67
Sandro Gerber <sandro-gerber@outlook.com>
2021-02-21 23:23:55 +0100

    Remove authentication with vuex store

--- store/index.ts
+++
@@ -1,7 +0,0 @@
-import { Store } from 'vuex'
-import { initialiseStores } from '~/helpers/store-accessor'
-
-const initializer = (store: Store<any>) => initialiseStores(store)
-
-export const plugins = [initializer]
-export * from '~/helpers/store-accessor'
