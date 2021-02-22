0b3dffcaa8672aec0d6a770f695e2d0e6dba11fb
Sandro Gerber <sandro-gerber@outlook.com>
2021-02-22 20:36:53 +0100

    Implement users list

--- pages/users.vue
+++ pages/users.vue
@@ -1,14 +1,34 @@
 <template>
   <v-container>
     <h1>Users</h1>
+    <ul>
+      <li v-for="user in users" :key="user.userId">
+        {{ user.username }} - {{ user.firstname }} {{ user.lastname }}
+      </li>
+    </ul>
   </v-container>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
+import { AxiosResponse } from 'axios'
+import UserModel from '~/models/data/UserModel'
+// import
 
 @Component({
   middleware: ['auth'],
 })
-export default class Users extends Vue {}
+export default class Users extends Vue {
+  private users: UserModel[] = []
+
+  mounted() {
+    this.fetchUsers()
+  }
+
+  async fetchUsers() {
+    await this.$axios.get('/user').then((res: AxiosResponse<UserModel[]>) => {
+      this.users = res.data
+    })
+  }
+}
 </script>
